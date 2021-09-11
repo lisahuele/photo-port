08ab96898dccf67a793ac6d6c910df7d66355901
lisahuele <lisahuele@gmail.com>
2021-09-11 15:46:54 -0400

    photos

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
+        <Gallery currentCategory={currentCategory} />
         <About></About>
       </main>
     </div>
